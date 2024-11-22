2d249c1338cca7f15cd1ec3b13f5b815efa4d63d
KharlAbban <khvngkharl123@gmail.com>
2024-11-22 08:18:19 +0000

    upate: Simple fix to error

--- data_api/api/index.js
+++ data_api/api/index.js
@@ -2,6 +2,12 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // Ensure the path matches your db.json location
 const middlewares = jsonServer.defaults();
+const cors = require("cors");
+
+server.use(cors({
+    origin: "https://prodev-psi.vercel.app/",
+    credentials: true
+}));
 
 server.use(middlewares);
 server.use(router);
